d9f0fe40793195c40452df9393e558427f9069ea
Richard Thompson <ithompson@hbku.edu.qa>
2020-07-07 14:21:00 +0300

    Added throw and warning for IHW BH correction

--- src/IHW.cpp
+++ src/IHW.cpp
@@ -32,8 +32,18 @@ void ihw(string fileName, SetupComp SetupValues)
 
 	if (SetupValues.getVerbose())
 		cout << "\nRscript call:\n" << cmd << endl << endl;
-	int sys = system(cmd.c_str());
+	//int sys = system(cmd.c_str());
 	//cout << "system: " << sys <<endl;
+	std::array<char, 128> buffer;
+	    std::string result;
+	FILE *pipe = popen(cmd.c_str(), "r");
+    while (fgets(buffer.data(), 128, pipe) != NULL) {
+        std::cout << "\t\tReading..." << std::endl;
+        result += buffer.data();
+    }
+    auto returnCode = pclose(pipe);
+    if ( result.find("Only 1 bin") == string::npos)
+    	throw std::invalid_argument("\nIHW internal BH correction inaccurate!\nPlease re-run with BH option!");
 	cerr << "\t" << flush;
 	completed();
 }
